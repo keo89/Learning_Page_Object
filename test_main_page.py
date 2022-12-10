3a8a15df3304f093e82456e2ce4b6b8dab065135
Olga <keo89@inbox.ru>
2022-12-10 18:45:54 +0300

    Добавлено два теста на отображение страницы регистрации

--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,9 @@
 from selenium.webdriver.common.by import By
 from .pages.main_page import MainPage
 
+
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
+    link = "http://selenium1py.pythonanywhere.com"
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
     page.open()                      # открываем страницу
     page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
@@ -12,3 +13,10 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+def test_guest_should_be_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com"
+    page = MainPage(browser, link)
+    page.open()
+    login_page = page.go_to_login_page()
+    login_page.should_be_login_page()
